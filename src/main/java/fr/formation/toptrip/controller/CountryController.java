df35fe46a07e2e31da02f07b894a859c27f38949
dalila57 <dalila.bousri@gmail.com>
2017-06-04 12:25:27 +0200

    modif countrycontroller : retour à la version original

--- src/main/java/fr/formation/toptrip/controller/CountryController.java
+++ src/main/java/fr/formation/toptrip/controller/CountryController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,24 +19,15 @@ import fr.formation.toptrip.entity.Country;
 
 @Controller
 @RequestMapping("/country")
-@SessionAttributes({ "countryList"})
 public class CountryController {
-	
-	private static final Logger LOGGER = LoggerFactory.getLogger(CountryController.class);
 	
 	@Autowired
 	private CountryRepository countryRepository;
 	
-	@ModelAttribute
-	public List<Country> countryList () {
-		return new ArrayList<>();
-	}
-
 	@RequestMapping("/index")
-	public ModelAndView showCountry(@RequestParam final String countryName){
+	public ModelAndView index() {
 		final ModelAndView mav = new ModelAndView("country");
 		mav.getModel().put("countryList", this.countryRepository.findAll());
-		CountryController.LOGGER.warn("liste country classe country :", countryList());
 		
 		return mav;
 	}
